61fdc8f9559d58590fb895ea22bdc2769f00e6ab
emilia <thaynaraswc@gmail.com>
2018-03-15 17:48:53 -0300

    primeiro commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header text-center">{{ __('SAPC - Sistema de alvará da Polícia Civil') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                    <div  style="align-items: center; display: flex; flex-direction: row; flex-wrap: wrap; justify-content: center;">
+                        <img  class="mb-4" style="vertical-align: middle;" src="img/bto.png" alt="" width="72" height="72">
+                    </div>
+                    @csrf
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Senha') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmar senha') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div  style="align-items: center; display: flex; flex-direction: row; flex-wrap: wrap; justify-content: center;">
+                            
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Criar conta') }}
+                                </button>
+                            
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/home.css">
+        <title>SAPC</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+
+    </head>
+    <body class="text-center">
+
+    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+    @if (Route::has('login'))
+      <header class="masthead mb-auto">
+        <div class="inner">
+          <h3 class="masthead-brand">SAPC </h3>
+          <nav class="nav nav-masthead justify-content-center">
+          @auth
+            <a class="nav-link active"  href="{{ url('/home') }}">Início</a>
+          @else
+            <a class="nav-link" href="{{ route('login') }}">Entrar</a>
+            <a class="nav-link" href="{{ route('register') }}">Criar conta</a>
+            @endauth
+          </nav>
+        </div>
+         @endif
+      </header>
+
+      <main role="main" class="inner cover">
+        <img src="img/bto.png" width="30%" height="30%">
+        <h1 class="cover-heading">SAPC - Sistema de alvará da Polícia Civil</h1>
+        <p class="lead">
+        Os interessados na obtenção de qualquer alvará deverão se dirigir à Gerência, munidos com a documentação para a abertura do processo e da taxa que deverá ser impressa através do site da Secretaria da Fazenda do Estado do Tocantins.</p>
+        <p class="lead">
+
+            <button class="btn btn-lg btn-secondary" type="button" data-toggle="modal" data-target=".bd-example-modal-lg">Mais informações</button>
+          
+        </p>
+      </main>
+        <!-- modal de informações -->
+      <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" style="color: #000;">Informações de contato</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body" style="color:#000; text-align: left;">
+                <p>Delegado(a): DPC Antonio Gonçalves de Carvalho Neto</p>
+                <p>Telefone(s): (63) 3218-6811</p>
+                <p>Email: costumes@ssp.to.gov.br</p>
+                <p>Endereço: Complexo II de Delegacias Especializadas -
+                 Av. Teotônio Segurado, Qd. 202 sul, Conj.01, Lt.04 - Centro</p>
+                 <p style="text-align: center">Palmas - Tocantins</p>
+
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>
+                
+              </div>
+            </div>
+        </div>
+    </div>
+
+      <footer class="mastfoot mt-auto">
+        <div class="inner">
+          <p>Desenvolvido por <a href="#">Emília</a>, 2018</p>
+        </div>
+      </footer>
+    </div>
+            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
+
+</html>
