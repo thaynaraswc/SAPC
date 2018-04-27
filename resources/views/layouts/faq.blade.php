ce40fb303ef81efbadc971f2e655b8e0aa988941
emilia <thaynaraswc@gmail.com>
2018-04-27 12:01:47 -0300

    inclusao de informacao FAQ

--- resources/views/layouts/faq.blade.php
+++ resources/views/layouts/faq.blade.php
@@ -5,33 +5,81 @@
         <div id="accordion">
             <div class="card">
                 <div class="card-header" id="headingOne">
-                <h3 class="mb-0">
-                    <button class="btn btn-link titulo-faq" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    Documentos necessários
+                <h5 class="mb-0">
+                    <button class="btn btn-link collapsed titulo-faq" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                   Documentos Necessários
                     </button>
-                </h3>
+                </h5>
                 </div>
+                <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                    <p>
+                        <a class="btn btn-outline-primary" data-toggle="collapse" href="#licencaUsoExplosivo" role="button" aria-expanded="false" aria-controls="licencaUsoExplosivo">
+                        LICENÇA PARA USO DE EXPLOSIVO
+                        </a>
+                        <a class="btn btn-outline-primary" data-toggle="collapse" href="#licencaUsoExplosivo" role="button" aria-expanded="false" aria-controls="licencaUsoExplosivo">
+                        FORNECEDORA, LOCADORA E OU INSTALADORA DE
+                        SISTEMA DE ALARME E MONITORAMENTO
+                        </a>
+                        <a class="btn btn-outline-primary" data-toggle="collapse" href="#licencaUsoExplosivo" role="button" aria-expanded="false" aria-controls="licencaUsoExplosivo">
+                        HOTEL, MOTEL, POUSADA, PENSÃO E SIMILARES
+                        </a>
+                    </p>
+                    <div class="collapse" id="licencaUsoExplosivo">
+                        <div class="card card-body" style="color: #000; text-align: left">
+                            <ol>
+                                <li>
+                                    Requerimento firmado pelo responsável;
+                                </li>
 
-                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-                <div class="card-body" style="color: #000; text-align: left">
-                    Para solicitação/emissão de alvará os dacumentos necessários são:
-                    <div style="text-align: left">
-                        <ul>
-                            <li>
-                                Documento de identificação com foto
-                            </li>
-                            <li>
-                                Documento de identificação com foto
-                            </li>
-                            <li>
-                                Documento de identificação com foto
-                            </li>
-                            <li>
-                                Documento de identificação com foto
-                            </li>
-                        </ul>
+                                <li>
+                                    CNPJ (Receita Federal) ou Declaração de Micro-Empresa;
+                                </li>
+
+                                <li>
+                                    Inscrição Estadual (JUCETINS);
+                                </li>
+
+                                <li>
+                                    Alvará de Localização e Funcionamento expedido pelo Município;
+                                </li>
+
+                                <li>
+                                    Cópias dos documentos pessoais dos sócios da empresa;
+                                </li>
+
+                                <li>
+                                    Título de Registro do Exército Brasileiro relativos aos itens 1.2.4 e 1.2.5
+                                </li>
+
+                                <li>
+                                    Certificado de Registro do Exército Brasileiro relativos aos demais itens;
+                                </li>
+
+                                <li>
+                                    Relação de produtos explosivos que contém estocado em paiol;
+                                </li>
+
+                                <li>
+                                    Relação dos funcionários que manuseiam explosivos com respectiva fotocópia
+                                    do certificado da carteira blaster;
+                                </li>
+
+                                <li>
+                                    Certificado de Licença de Funcionamento expedido pela Polícia Federal,
+                                    tratando-se do exercício de atividade não eventual com produtos químicos
+                                    controlados;
+                                </li>
+
+                                <li>
+                                    Vistoria Policial Civil;
+                                </li>
+
+                                <li>
+                                    Comprovante de Pagamento da Taxa de Serviços Estaduais.
+                                </li>
+                            </ol>
+                        </div>
                     </div>
-                </div>
                 </div>
             </div>
             <div class="card">
@@ -43,7 +91,7 @@
                 </h5>
                 </div>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-                <div class="card-body">
+                <div class="card-body" style="color: #000; text-align: left">
                     Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
                 </div>
                 </div>
@@ -57,23 +105,48 @@
                 </h5>
                 </div>
                 <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-                <div class="card-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                <div class="card-body" style="color: #000; text-align: left">
+                A vistoria será executada por Policial Civil designado pela Autoridade Policial Titular da
+                            Delegacia de Polícia competente para o ato e se dará após a apresentação da documentação
+                            exigida para a atividade desejada, objetivando verificar se o empreendimento ou atividade se
+                            enquadra na categoria declarada pelo requerente e atende aos requisitos legais do Código de
+                            Posturas do Município, se houver, e legislação correspondente.
+                    <ul>
+                        <li>
+                            Após conferir os critérios técnicos acima relacionados, o policial fará relatório
+                            circunstanciado acompanhado de levantamento fotográfico, emitindo o seu parecer dirigido ao
+                            Delegado de Polícia responsável pela emissão do Alvará.
+                        </li>
+                    </ul>
                 </div>
                 </div>
             </div>
             <div class="card">
                 <div class="card-header" id="headingThree">
                 <h5 class="mb-0">
-                    <button class="btn btn-link collapsed titulo-faq" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    <button class="btn btn-link collapsed titulo-faq" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
                     Alvará
                     </button>
                 </h5>
                 </div>
-                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-                <div class="card-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                </div>
+                <div id="collapseFour" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                    <div class="card-body" style="color: #000; text-align: left">
+                        <ul>
+                            <li>
+                                O <strong>Alvará Anual de Licença</strong> terá validade durante o ano civil para o qual for expedido,
+                                podendo ser revalidado até o último dia do mês de janeiro do ano civil subsequente.
+                            </li>
+                            <li>
+                                O <strong>Alvará de Licença Mensal</strong> terá validade de trinta dias a contar da data de sua
+                                expedição.
+                            </li>
+                            <li>
+                                O <strong>Alvará de Licença Diário</strong> terá validade de vinte e quatro horas, a partir de sua
+                                expedição, devendo ser solicitado no prazo de, no mínimo, quarenta e oito horas antes do início das
+                                atividades para as quais é pleiteada.
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
