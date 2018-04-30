88dee221558467c88e6812866c31b4e691561f55
emilia <thaynaraswc@gmail.com>
2018-04-30 08:32:54 -0300

    inserindo docs necessários faq

--- resources/views/layouts/faq.blade.php
+++ resources/views/layouts/faq.blade.php
@@ -16,7 +16,7 @@
                         <a class="btn btn-outline-primary" data-toggle="collapse" href="#licencaUsoExplosivo" role="button" aria-expanded="false" aria-controls="licencaUsoExplosivo">
                         LICENÇA PARA USO DE EXPLOSIVO
                         </a>
-                        <a class="btn btn-outline-primary" data-toggle="collapse" href="#licencaUsoExplosivo" role="button" aria-expanded="false" aria-controls="licencaUsoExplosivo">
+                        <a class="btn btn-outline-primary" data-toggle="collapse" href="#licencaSistemaDeAlarme" role="button" aria-expanded="false" aria-controls="licencaSistemaDeAlarme">
                         FORNECEDORA, LOCADORA E OU INSTALADORA DE
                         SISTEMA DE ALARME E MONITORAMENTO
                         </a>
@@ -26,6 +26,65 @@
                     </p>
                     <div class="collapse" id="licencaUsoExplosivo">
                         <div class="card card-body" style="color: #000; text-align: left">
+                        <h3 style="text-align: center">LICENÇA PARA USO DE EXPLOSIVO</h3>
+                            <ol>
+                                <li>
+                                    Requerimento firmado pelo responsável;
+                                </li>
+
+                                <li>
+                                    CNPJ (Receita Federal) ou Declaração de Micro-Empresa;
+                                </li>
+
+                                <li>
+                                    Inscrição Estadual (JUCETINS);
+                                </li>
+
+                                <li>
+                                    Alvará de Localização e Funcionamento expedido pelo Município;
+                                </li>
+
+                                <li>
+                                    Cópias dos documentos pessoais dos sócios da empresa;
+                                </li>
+
+                                <li>
+                                    Título de Registro do Exército Brasileiro relativos aos itens 1.2.4 e 1.2.5
+                                </li>
+
+                                <li>
+                                    Certificado de Registro do Exército Brasileiro relativos aos demais itens;
+                                </li>
+
+                                <li>
+                                    Relação de produtos explosivos que contém estocado em paiol;
+                                </li>
+
+                                <li>
+                                    Relação dos funcionários que manuseiam explosivos com respectiva fotocópia
+                                    do certificado da carteira blaster;
+                                </li>
+
+                                <li>
+                                    Certificado de Licença de Funcionamento expedido pela Polícia Federal,
+                                    tratando-se do exercício de atividade não eventual com produtos químicos
+                                    controlados;
+                                </li>
+
+                                <li>
+                                    Vistoria Policial Civil;
+                                </li>
+
+                                <li>
+                                    Comprovante de Pagamento da Taxa de Serviços Estaduais.
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                    <div class="collapse" id="licencaSistemaDeAlarme">
+                        <div class="card card-body" style="color: #000; text-align: left">
+                        <h3 style="text-align: center">FORNECEDORA, LOCADORA E OU INSTALADORA DE
+                        SISTEMA DE ALARME E MONITORAMENTO</h3>
                             <ol>
                                 <li>
                                     Requerimento firmado pelo responsável;
